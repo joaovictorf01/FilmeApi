a724a4bfdf9261c3193e23ed28c7db37f3e0199b
joão victor <joaovictorf01@gmail.com>
2022-07-25 05:25:38 -0300

    adicionando o get por id

--- FilmeApi/Controllers/FilmeController.cs
+++ FilmeApi/Controllers/FilmeController.cs
@@ -6,23 +6,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace FilmesApi.Controllers
-{
-    [ApiController]
-    [Route("[controller]")]
-    public class FilmeController : ControllerBase
-    {
-        private static List<Filme> filmes = new List<Filme>();
+namespace FilmesApi.Controllers;
 
-        [HttpPost]
-        public void AdicionaFilme([FromBody] Filme filme)
-        {
-            filmes.Add(filme);
-        }
-        [HttpGet]
-        public IEnumerable<Filme> RecuperaFilmes()
-        {
-            return filmes;
-        }
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+    private static List<Filme> filmes = new List<Filme>();
+
+    [HttpPost]
+    public void AdicionaFilme([FromBody] Filme filme)
+    {
+        filmes.Add(filme);
     }
+    [HttpGet]
+    public IEnumerable<Filme> RecuperaFilmes()
+    {
+        return filmes;
+    }
+[HttpGet("{id}")]
+    public Filme RecuperaFilme(int id)
+    {
+        return filmes.FirstOrDefault(f => f.Id == id);
+    }
+        
 }
